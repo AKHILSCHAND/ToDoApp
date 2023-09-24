271e406bf1da03317a400c78f49b4d03b67a4f85
akhilschand <nandhuakhil95@gmail.com>
2023-09-24 13:33:45 +0530

    mark as done and mark as undone functionality

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('add_task', views.add_task, name='add_task'),
+    path('mark_done/<int:pk>', views.mark_done, name='mark_done'),
+    path('mark_undone/<int:pk>', views.mark_undone, name='mark_undone'),
     
     
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from todoapp.models import Task
 
@@ -9,3 +9,19 @@ def add_task(request):
     task = request.POST['task']
     Task.objects.create(task=task)
     return redirect('home')
+
+
+def mark_done(request,pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
+
+
+def mark_undone(request,pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = False
+    task.save()
+    return redirect('home')
+
+
